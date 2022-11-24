fc988dceefbd8576c4ba0947d62962d4bc31c6d2
Ottocomic <ottocreaciones@gmail.com>
2022-11-24 16:42:02 +0100

    Funciones Ejercicio 4

---
+++ Ejercicios/Modulo_02/Ejercicio_04/main.js
@@ -0,0 +1,16 @@
+//Crea una función que reciba un número y muestre por consola si es par o impar.
+
+
+function parImpar(numero){
+resultado=numero%2
+if(resultado==0){
+    console.log('Es un número par');
+}else{
+
+}
+  console.log('Es un número impar');
+
+}
+
+parImpar(5)
+
