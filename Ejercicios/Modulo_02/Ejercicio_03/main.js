308bc303886d4d5501a6ef9f2a401782add24a8c
Ottocomic <ottocreaciones@gmail.com>
2022-11-24 16:31:51 +0100

    Funciones-Ejercicio 3

---
+++ Ejercicios/Modulo_02/Ejercicio_03/main.js
@@ -0,0 +1,18 @@
+//Crea una función que calcule el área de un rectángulo y la muestre por consola.
+
+function areaRectangulo(ladoA, ladoB){
+
+ const area= ladoA*ladoB;
+
+ console.log(area);
+
+ 
+}
+
+areaRectangulo(6, 2);
+
+console.log("Hola Mundo");
+
+
+
+
