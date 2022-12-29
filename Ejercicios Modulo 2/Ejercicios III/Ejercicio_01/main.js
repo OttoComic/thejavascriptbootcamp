cac905d27c735be294440846919c5f1fc927ddc4
Ottocomic <ottocreaciones@gmail.com>
2022-12-29 12:49:28 +0100

    ejercicio 1 corregido

--- Ejercicios Modulo 2/Ejercicios III/Ejercicio_01/main.js
+++ Ejercicios Modulo 2/Ejercicios III/Ejercicio_01/main.js
@@ -1,14 +1,12 @@
-//Crea un formulario con un input de tipo text y un botón de tipo submit.
-//Crea un párrafo vacío debajo del formulario.
 
-
-
-document.querySelector(".btn").addEventListener("click",()=>
+const parrafo=document.querySelector(".parrafo");
 //Al hacer click en el botón submit, pinta en el párrafo el texto que hay en el input.
-{document.querySelector("p").textContent=document.querySelector(".text").value;
+document.querySelector(".btn").addEventListener("click",(e)=>
+
+{  e.preventDefault()
+    parrafo.textContent=document.querySelector(".text").value;
 //Si el texto del input es menor de 5 caracteres, pinta el texto en rojo.
-document.querySelector(".text").value.length<5 ? document.querySelector("p").style.color='red': document.querySelector("p").style.color='black';
+    document.querySelector(".text").value.length<5 ? parrafo.style.color='red': parrafo.style.color='black';
 //Al hacer click en el botón submit, vacía el input.
-document.getElementsByClassName("text")[0].value = "";
+document.querySelector(".form").reset();
 });
-//condición ? expr1 : expr2
